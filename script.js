745a3a9d23ce28260f8f5e52fa9a9b3dd19604d7
rajatkinlekar <kinlekarrajat@gmail.com>
2025-07-25 01:26:48 +0530

    UI commit

---
+++ script.js
@@ -0,0 +1,24 @@
+class Cell {
+    
+    constructor(id, option) {
+        this.id = id;
+        this.option = option;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getOption() {
+        return this.option;
+    }
+
+    setId(id) {
+        this.id = id;
+    }
+
+    setOption(option) {
+        this.option = option;
+    }
+
+}
